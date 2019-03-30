241aa954ae70ee2440d93e2aceb4effc9e359db6
Artistern <1136223096@qq.com>
2019-03-30 15:18:01 +0800

    提示，目前只能使用32位色彩

--- MyGame/源.cpp
+++ MyGame/源.cpp
@@ -1,83 +1,165 @@
+//**************头文件******************
 #include "stdafx.h"
 #include <Windows.h>
-#include<Mmsystem.h>
-#include <winuser.h>
 #include "resource2.h"
 #include <ddraw.h>
-#include <cstdio>
-
+#include <iostream>
+//**************lib***********************
 #pragma comment(lib,"ddraw.lib")
-#pragma comment(lib,"winmm.lib")
 #pragma comment(lib,"dxguid.lib")
-HINSTANCE hinstance_pop;
+//*******************定义基础属性**********************
+#define SCREEN_WIDTH    1920  // size of screen
+#define SCREEN_HEIGHT   1080
+#define SCREEN_BPP      32    //这里我们只能使用32位色彩
+#define MAX_COLORS      256  // maximum colors
+#define WINDOW_CLASS_NAME "WINCLASS1"
+#define DD_INIT_STRUCT(ddstruct) { memset(&ddstruct,0,sizeof(ddstruct)); ddstruct.dwSize=sizeof(ddstruct); }
+//********************定义句柄*************************
+HINSTANCE		hinstance_pop=NULL;
+HWND			Main_Hwnd=NULL;
+//********************定义Direct绘图*******************
+LPDIRECTDRAW7			lpdd;
+DDSURFACEDESC2			ddsd;
+LPDIRECTDRAWSURFACE7	lpddsprimary=NULL;
+LPDIRECTDRAWPALETTE		lpddpal = NULL;   // a pointer to the created dd palette
+PALETTEENTRY          palette[256];          // color palette
+//********************定义辅助变量*********************
 int line = 0;
-POINT point;
-
-HWND Main_Hwnd;
-
-//directdraw7.0
-LPDIRECTDRAW lpdd;
-
-
-int Game_Init(void *parms=NULL)
+//********************初始化进程***********************
+int Game_Init(void *parms = NULL)
 {
-	PALETTEENTRY palet[256];
-
-	for(int color=0;color<256;color++)
-	{
-		palet[color].peGreen = rand() % 255;
-		palet[color].peBlue = rand() % 255;
-		palet[color].peRed = rand() % 255;
-
-		palet[color].peFlags = PC_NOCOLLAPSE;
-	}
-	palet[0].peGreen = 0;
-	palet[0].peBlue = 0;
-	palet[0].peRed = 0;
-	palet[0].peFlags = PC_NOCOLLAPSE;
-
-	palet[255].peGreen = 255;
-	palet[255].peBlue = 255;
-	palet[255].peRed = 255;
-	palet[255].peFlags = PC_NOCOLLAPSE;
-	//初始化
-	if(FAILED(DirectDrawCreateEx(NULL,(void**)&lpdd,IID_IDirectDraw7,NULL)))
-	{
-		return 0;
-	}
-	if(FAILED(lpdd->SetCooperativeLevel(Main_Hwnd, DDSCL_FULLSCREEN | DDSCL_ALLOWMODEX | DDSCL_ALLOWREBOOT | DDSCL_EXCLUSIVE)))
-	{
-		return 0;
-	}
-	if(FAILED(lpdd->SetDisplayMode(800, 600, 8)))
-	{
-		return 0;
-	}
-	LPDIRECTDRAWPALETTE lpddpal = NULL;
-	if(FAILED(lpdd->CreatePalette(DDPCAPS_8BIT|DDPCAPS_ALLOW256|DDPCAPS_INITIALIZE,palet, &lpddpal,NULL)))
+	
+	//创建一个DirectDraw对象，返回到lpdd
+	if (FAILED(DirectDrawCreateEx(NULL, (void **)&lpdd, IID_IDirectDraw7, NULL)))
 	{
 		return 0;
 	}
 	
+	//设置与windows协作的模式，目前为全屏模式
+	if (FAILED(lpdd->SetCooperativeLevel(Main_Hwnd,
+		DDSCL_FULLSCREEN | DDSCL_ALLOWMODEX |
+		DDSCL_EXCLUSIVE | DDSCL_ALLOWREBOOT)))
+	{
+		return(0);
+	} 
+
+	;
+	//设置视频模式,640*480*8,错误点
+	if (FAILED(lpdd->SetDisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, 0, 0)))
+	{
+		return(0);
+	}
+	//清空ddsd并且设置大小
+	memset(&ddsd, 0, sizeof(ddsd));
+	ddsd.dwSize = sizeof(ddsd);
+
+	ddsd.dwFlags = DDSD_CAPS;
+	//这里表示创建的是主表面
+	ddsd.ddsCaps.dwCaps = DDSCAPS_PRIMARYSURFACE;
+	//创建主显示表面
+	if(FAILED(lpdd->CreateSurface(&ddsd,&lpddsprimary,NULL)))
+	{
+		//isFail = false;
+		return 0;
+	}
+	for (int color = 1; color < 255; color++)
+	{
+		// fill with random RGB values
+		palette[color].peRed = rand() % 256;
+		palette[color].peGreen = rand() % 256;
+		palette[color].peBlue = rand() % 256;
+
+		// set flags field to PC_NOCOLLAPSE
+		palette[color].peFlags = PC_NOCOLLAPSE;
+	} // end for color
+
+// now fill in entry 0 and 255 with black and white
+	palette[0].peRed = 0;
+	palette[0].peGreen = 0;
+	palette[0].peBlue = 0;
+	palette[0].peFlags = PC_NOCOLLAPSE;
+
+	palette[255].peRed = 255;
+	palette[255].peGreen = 255;
+	palette[255].peBlue = 255;
+	palette[255].peFlags = PC_NOCOLLAPSE;
+	//创建调色板
+	if(FAILED(lpdd->CreatePalette(DDPCAPS_8BIT|
+								DDPCAPS_ALLOW256|
+								DDPCAPS_INITIALIZE,
+								palette,&lpddpal,NULL)))
+	{
+		return 0;
+	}
+	//指定调色板
+	if(FAILED(lpddsprimary->SetPalette(lpddpal)))
+	{
+		return 0;
+	}
 	return 1;
 }
+//*******************主进程****************************
 int Game_Main(void *parms=NULL)
 {
-	//主进程
-	if (KEYDOWN(VK_ESCAPE))
+	if(KEYDOWN(VK_ESCAPE))
 	{
 		PostMessage(Main_Hwnd, WM_DESTROY, 0, 0);
 	}
+	memset(&ddsd, 0, sizeof(ddsd));
+	ddsd.dwSize = sizeof(ddsd);
+
+	memset(&ddsd, 0, sizeof(ddsd));
+	ddsd.dwSize = sizeof(ddsd);
+
+	if(lpddsprimary==nullptr)
+	{
+		return  0;
+	}
+	if (FAILED(lpddsprimary->Lock(nullptr, &ddsd,
+		DDLOCK_SURFACEMEMORYPTR | DDLOCK_WAIT,
+		nullptr)))
+	{
+		return(0);
+	}
+	int mempitch = (int)ddsd.lPitch;
+	UCHAR *video_buffer = (UCHAR *)ddsd.lpSurface;
+
+	for (int index = 0; index < 1000; index++)
+	{
+		UCHAR color = rand() % 256;
+		int x = rand() % SCREEN_HEIGHT;
+		int y = rand() % SCREEN_WIDTH;
+
+		video_buffer[x + y * (mempitch>>1)] = color;
+	}
+
+	if (FAILED(lpddsprimary->Unlock(NULL)))
+		return(0);
+
+	Sleep(30);
+	return(1);
+
+	//主进程
 	return 1;
 }
+//*******************游戏结束的操作********************
 int Game_Shutdown(void *parms=NULL)
 {
-	if(lpdd)
+	if (lpddpal)
+	{
+		lpddpal->Release();
+		lpddpal = NULL;
+	}
+	if (lpddsprimary)
+	{
+		lpddsprimary->Release();
+		lpddsprimary = NULL;
+	}
+	if (lpdd)
 	{
 		lpdd->Release();
 		lpdd = NULL;
-
-	}
+	} // end if
 	//结束
 	return 1;
 }
@@ -87,47 +169,20 @@ LRESULT CALLBACK WindowPro(HWND hwnd,
 							WPARAM wparam,
 							LPARAM lparam)
 {
-	PAINTSTRUCT ps;
-	HDC hdc;
-
+	PAINTSTRUCT		ps;		// used in WM_PAINT
+	HDC				hdc;	// handle to a device context
 	switch(msg)
 	{
 	case WM_CREATE:
 		{
 		return 0;
 	}break;
-	case WM_COMMAND:
-		{
-			switch (LOWORD(wparam))
-			{
-			case MENU_FILE_ID_OPEN:
-				{
-				PlaySound(MAKEINTRESOURCE(IDR_WAVE1), hinstance_pop, SND_ASYNC | SND_RESOURCE);
-				}break;
-			case MENU_FILE_ID_CLOSE:
-				{
-				PlaySound(NULL, hinstance_pop, SND_PURGE);
-			}break;
-			default:break;
-			}
-		}break;
 	case WM_PAINT:
 		{
-		//GetWindowRect(hwnd, &rect);
-		//width = rect.right - rect.left;
-		//high = rect.top - rect.bottom;
-		/*hdc = BeginPaint(hwnd, &ps);
-		SetTextColor(hdc, RGB(rand() % 255, rand() % 255, rand() % 255));
-		SetBkColor(hdc, RGB(rand() % 255, rand() % 255, rand() % 255));
-		SetBkMode(hdc, TRANSPARENT);
-		TextOut(hdc, rand() % 400, rand() % 400, TEXT("GDI TEXT DOME"), sizeof("GDITEXTDOME"));
-		EndPaint(hwnd, &ps);*/
-		return 0;
-	}break;
-	case WM_CLOSE:
-	{		
-		return (DefWindowProc(hwnd, msg, wparam, lparam));	
-	}break;
+		hdc = BeginPaint(hwnd, &ps);
+
+		EndPaint(hwnd, &ps);
+		}break;
 	case WM_DESTROY:
 		{
 			PostQuitMessage(0);//产生一个退出消息
@@ -170,24 +225,23 @@ int WINAPI WinMain(
 	}
 	//创建窗口的操作
 	if (!(hwnd = CreateWindowEx(NULL, 
-				winclass.lpszClassName, 
+				TEXT(WINDOW_CLASS_NAME),
 				TEXT("游戏控制台"), 
-				WS_POPUP|WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+				WS_POPUP | WS_VISIBLE,
+				//WS_POPUP,
 				0, 0, 
-				400, 400, 
+				SCREEN_WIDTH, SCREEN_HEIGHT, 
 				NULL, 
-				LoadMenu(hinstance,TEXT("MainMenu")), 
+				NULL,
 				hinstance, 
 				NULL)))
 	{
 		return 0;
 	}
 	Main_Hwnd = hwnd;
-	srand(GetTickCount());
 	Game_Init();
 	while (true)
 	{
-		int startTime = GetTickCount();
 		if(PeekMessage(&msg,NULL,0,0,PM_REMOVE))//如果消息队列里面没有对应的消息，就直接执行下面的程序
 		{
 			if(msg.message==WM_QUIT)//当消息队列中产生一个退出指令
@@ -198,7 +252,6 @@ int WINAPI WinMain(
 			DispatchMessage(&msg);//调用winProc
 		}
 		Game_Main();
-		while ((GetTickCount()- startTime)<33);
 	}
 	Game_Shutdown();
 	return (msg.wParam);
